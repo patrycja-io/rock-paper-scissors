662d9c6788904f95ec6719ea3e379c6a88f25968
Patrycja <patrycja@dynium.ai>
2022-03-30 20:20:07 +0100

    fixed second version

--- app.js
+++ app.js
@@ -1,52 +1,33 @@
-const resultDisplay = document.querySelector("#reslut");
-const choicesDisplay = document.querySelector("#choices");
-const choices = ["ROCK", "PAPER", "SCISSORS"];
+const resultDisplay = document.querySelector('#result')
+const choicesDisplay = document.querySelector('#choices')
+const choices = ['rock', 'paper', 'scissors']
 
 const handleClick = (e) => {
-  getResults(
-    e.target.innerHTML,
-    choices[Math.floor(Math.random() * choices.length)]
-  );
-};
-
+  getResults(e.target.innerHTML, choices[Math.floor(Math.random() * choices.length)])
+}
 choices.forEach(choice => {
-  const button = document.createElement("button");
-  button.innerHTML = choice;
-  button.addEventListener("click", handleClick);
-  choicesDisplay.appendChild(button);
-});
+  const button = document.createElement('button')
+  button.innerHTML = choice
+  button.addEventListener('click', handleClick)
+  choicesDisplay.appendChild(button)
+})
 
 const getResults = (userChoice, computerChoice) => {
   switch (userChoice + computerChoice) {
-    case "SCISSORSPAPER":
-    case "ROCKSCISSORS":
-    case "PAPERROCK":
-      resultDisplay.innerHTML =
-        "You chose" +
-        userChoice +
-        "and the computer chose" +
-        computerChoice +
-        ", so you Win!";
-      break;
-    case "PAPERSCISSORS":
-    case "SCISSORSROCK":
-    case "ROCKPAPER":
-      resultDisplay.innerHTML =
-        "You chose" +
-        userChoice +
-        "and the computer chose" +
-        computerChoice +
-        ", so you Lose!";
-      break;
-    case "PAPERPAPER":
-    case "SCISSORSSCISSORS":
-    case "ROCKROCK":
-      resultDisplay.innerHTML =
-        "You chose" +
-        userChoice +
-        "and the computer chose" +
-        computerChoice +
-        ", its a draw!";
-      break;
+    case 'scissorspaper':
+    case 'rockscissors':
+    case 'paperrock':
+      resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , YOU WIN!'
+      break
+    case 'paperscissors':
+    case 'scissorsrock':
+    case 'rockpaper':
+      resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , YOU LOSE!'
+      break
+    case 'scissorsscissors':
+    case 'rockrock':
+    case 'paperpaper':
+      resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , ITS A DRAW!'
+      break
   }
 }
